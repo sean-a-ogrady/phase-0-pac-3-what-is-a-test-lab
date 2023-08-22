09654e8d8eff861669a9ea56e52d521777318a14
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-08-21 21:13:39 -0400

    Complete tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = Number.MIN_VALUE; //Susan is very short
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
